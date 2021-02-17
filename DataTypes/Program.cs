3a5d18f0009527cc6d01d3604ba515c9bb5f627c
Adam Macken <mackenadam@gmail.com>
2021-02-17 17:53:51 +1300

    Constants

--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -4,8 +4,17 @@ namespace DataTypes
 {
     class Program
     {
+        // Explore immutable values
+        // Constants as fields
+        const double PI = 3.14159265359;
+        const int weeks = 52, months = 12;
+        const string myBirthday = "3 July 1989";
+
         static void Main(string[] args)
         {
+            Console.WriteLine("My birthday is always going to be {0}", myBirthday);
+            Console.Read();
+
             // Challenge to create variables for each data type
             // Boolean - True or false
             bool myBoolean = true;
@@ -27,8 +36,6 @@ namespace DataTypes
             char myChar = 'a';
             // String
             string myString = "This is a string";
-
-            // Explore immutable values
         }
     }
 }
