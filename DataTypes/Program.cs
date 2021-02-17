ecc0d9f793ddf2322becf46122f02f911285d283
Adam Macken <mackenadam@gmail.com>
2021-02-17 17:48:10 +1300

    Explore constants

--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -7,36 +7,28 @@ namespace DataTypes
         static void Main(string[] args)
         {
             // Challenge to create variables for each data type
-
             // Boolean - True or false
             bool myBoolean = true;
-
             // SByte - -128 -> 127
             sbyte mySByte = 127;
-
             // Short - -32768 -> 32767
             short myShort = 32766;
-
             // Int
             int myInteger = 5;
-
             // Long
             long myLong = 1234567890L;
-
             // Float
             float myFloat = 32.46F;
-
             // Double
             double myDouble = 32.46D;
-            
             // Decimal
             decimal myDecimal = 53005.25M;
-
             // Char
             char myChar = 'a';
-
             // String
             string myString = "This is a string";
+
+            // Explore immutable values
         }
     }
 }
