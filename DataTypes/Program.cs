e5a52a9cd62fe8ff375a8d1602800f092a979f80
Adam Macken <mackenadam@gmail.com>
2021-02-17 17:28:21 +1300

    Add project files.

---
+++ DataTypes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Challenge to create variables for each data type
+        }
+    }
+}
