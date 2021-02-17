258d8e6cbcc062ada446ee52fec19bc22ab8c8b3
Adam Macken <mackenadam@gmail.com>
2021-02-17 17:35:18 +1300

    Boolean, SByte, Short

--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -7,6 +7,15 @@ namespace DataTypes
         static void Main(string[] args)
         {
             // Challenge to create variables for each data type
+
+            // Boolean - True or false
+            bool myBoolean = true;
+
+            // SByte - -128 -> 127
+            sbyte mySByte = 127;
+
+            // Short - -32768 -> 32767
+            short myShort = 32766;
         }
     }
 }
